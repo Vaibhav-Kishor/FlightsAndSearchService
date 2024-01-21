81d02e0adaf6e12e35d57d9a966188db26b9bd63
Vaibhav-Kishor <vaibhavkishor7@gmail.com>
2024-01-21 15:46:44 +0530

    Added city model and migration

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { PORT } = require('./config/serverConfig');
+const { PORT } = require('../../serverConfig');
 const bodyParser = require('body-parser');
 
 const setupAndStartServer = async () =>{
